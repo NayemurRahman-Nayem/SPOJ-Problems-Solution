2cfb87e66405a5850d5d40560a0c08e36eca6d89
Nayemur Rahman Nayem <48326626+NayemurRahman-Nayem@users.noreply.github.com>
2023-11-08 16:53:21 +0600

    Create Prime or Not.cpp

---
+++ Prime or Not.cpp
@@ -0,0 +1,121 @@
+// Bismillahir Rahmanir Rahim
+ 
+ 
+#include <bits/stdc++.h>
+using namespace std ;
+typedef long long ll ;
+typedef long double ld ;
+using u64 = uint64_t;
+using u128 = __uint128_t;
+typedef unsigned long long ull ;
+typedef vector<int> vi ;
+typedef vector<ll> vl ;
+typedef vector<vi> vvi ;
+typedef vector<vl> vvl ;
+typedef pair<int, int> pii ;
+typedef pair<ll, ll> pll ;
+typedef pair<float, float> pff ;
+typedef pair<double, double> pdd ;
+typedef pair<ll, ll> pll ;
+#define pb push_back
+#define el << '\n' ;
+#define all(a) (a).begin(),(a).end()
+#define allr(a) (a).begin(),(a).end()
+#define F first
+#define S second
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds ;
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+#define pb push_back
+#define PI 3.14159265358979323
+#define file() freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+#define fastIO	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0) ;
+ull bin_expo(ull base, ull pow, ull m) {
+    if (pow == 0) return 1;ull ret = bin_expo(base, pow / 2, m) % m ;
+    if (pow & 1) return (ret % m * ret % m * base % m) % m;else return (ret % m * ret % m) % m ; }
+ll mod_add(ll a, ll b, ll m) { return ((a % m) + (b % m)) % m ; }
+ll mod_sub(ll a, ll b, ll m) { return (a-b+m)%m ; }
+ll mod_mul(ll a, ll b, ll m) { return ((a % m) * (b % m)) % m ; }
+ll mod_div(ll a, ll b, ll m) { return ((a % m) * bin_expo(b, m - 2, m)) % m ; }
+ll aModB(ll a , ll b) { if(a<0 ) { return ((a+(abs(a)/b)*b)+b)%b ; } else { return a%b; } }
+ll lcm(ll a, ll b) { return (a * b) / __gcd(a, b) ; }
+int dx[] = {0, 0, +1, -1, +1, -1, -1, +1} ;
+int dy[] = {+1, -1, 0, 0, +1, -1, +1, -1} ;
+const ll infLL = 4e18 + 123 ;
+const ll inf = 1e12 + 7 ;
+const ll mod = 1e9 + 7 ;
+const ll mod2 = 998244353 ;
+const ll mx = 1e6 + 10 ;
+ll LOG = 21 ; 
+ 
+// ---------------------------------------------------------------------------------------------------------------------
+ 
+ 
+u64 binpower(u64 base, u64 e, u64 mod) {
+    u64 result = 1;
+    base %= mod;
+    while (e) {
+        if (e & 1)
+            result = (u128)result * base % mod;
+        base = (u128)base * base % mod;
+        e >>= 1;
+    }
+    return result;
+}
+ 
+bool check_composite(u64 n, u64 a, u64 d, int s) {
+    u64 x = binpower(a, d, n);
+    if (x == 1 || x == n - 1)
+        return false;
+    for (int r = 1; r < s; r++) {
+        x = (u128)x * x % n;
+        if (x == n - 1)
+            return false;
+    }
+    return true;
+};
+ 
+bool MillerRabin(u64 n) { // returns true if n is prime, else returns false.
+    if (n < 2)
+        return false;
+    int r = 0;
+    u64 d = n - 1;
+    while ((d & 1) == 0) {
+        d >>= 1;
+        r++;
+    }
+    for (int a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
+        if (n == a)
+            return true;
+        if (check_composite(n, a, d, r))
+            return false;
+    }
+    return true;
+}
+ 
+ 
+void solution(int cs) {
+    
+    ull n; 
+    cin >> n ; 
+    if(MillerRabin(n)) {
+        cout << "YES" el ; 
+    }
+    else {
+        cout << "NO" el ; 
+    }
+}             
+ 
+int main() { 
+    fastIO ;
+    // #ifndef ONLINE_JUDGE
+    // file() ;
+    // #endif
+    int t = 0 ; 
+    int cs = 1 ;
+    cin >> cs ;  
+    for(ll i=1;i<=cs;i++) solution(i) ; 
+    return 0 ;
+} 
